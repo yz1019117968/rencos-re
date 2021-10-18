500926bd3f615e19d33b1d640266307e272b008f
yz1019117968 <1019117968@qq.com>
2021-10-18 14:26:27 +0800

    semantic done

--- scripts/retrieve.bat
+++ scripts/retrieve.bat
@@ -8,8 +8,10 @@ set test_set_tgt=../samples/python/test/test.txt.tgt
 set query_out_path=../samples/python/test/test.vec.pkl
 set source_out_path=../samples/python/train/train.vec.pkl
 set simi_id_out=../samples/python/test/id_score
+set test_ref_src_1=../samples/python/test/test.ref.src.1
+set test_ref_tgt_1=../samples/python/test/test.ref.tgt.1
 
 rem python syntax python
 
 python ../semantic.py %model_path% %vocab_file% %train_set_src% %train_set_tgt% %test_set_src% %test_set_tgt% ^
-                      %query_out_path% %source_out_path% %simi_id_out%
+                      %query_out_path% %source_out_path% %simi_id_out% %test_ref_src_1% %test_ref_tgt_1%
